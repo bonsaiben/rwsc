69c429d63bbfc239d3fd8c3c7ae48b5b8f80edbb
Benjamin Sullivan <bsullivan2@gmail.com>
2012-06-05 13:11:09 +0900

    updates API version to valid version 2010-08-05

--- lib/rwsc/const.rb
+++ lib/rwsc/const.rb
@@ -4,7 +4,7 @@ module Rwsc
 
   # = this module is define constatns .
   module CONST
-    API_VERSION = '2010-09-15'
+    API_VERSION = '2010-08-05'
     USER_AGENT = "rwsc/#{Rwsc::VERSION::STRING}"
 
     WS_URL = "http://api.rakuten.co.jp/rws/3.0/rest"
